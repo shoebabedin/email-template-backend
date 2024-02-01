290d3f53df4c9c6d2ab86ffb49f517cc9abb8726
shoebabedin <shoeb.abedin@intelli.global>
2024-02-01 18:58:51 +0600

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require('express');
 const route = require('./routes');
 const mongodb = require('./config/config');
 const jwt = require("jsonwebtoken");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 const app = express();
